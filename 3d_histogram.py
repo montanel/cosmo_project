b8aa2fe1fdbd0d90b06660a8f5f7e990e39bc26d
Luca <luca@castor.home>
2017-11-01 19:08:55 +0100

    Nothing really

--- 3d_histogram.py
+++ 3d_histogram.py
@@ -11,22 +11,23 @@ parser = argparse.ArgumentParser()
 parser.add_argument("min", type=int, help="the lower x bound for plotting")
 parser.add_argument("max", type=int, help="the upper x bound for plotting")
 parser.add_argument("bins", type=int, help="the number of bins")
+parser.add_argument("data", type=int, help="the number of data points")
 args = parser.parse_args()
 
 xmin = args.min
 xmax = args.max
 nbins = args.bins
+ndata = args.data
 bin_size = (xmax-xmin)/float(nbins)
 
+
 #Setting the data
-data_rand = [random.gauss(6,1) for i in range(0,50000)]
-nb_data = len(data_rand)
+data = [random.gauss(6,1) for i in range(0,ndata)]
+
 
 #3d histogram fct for histogram
+#Computes a gaussian with mean: data_val and width: sigma on the entire grid: grid
 def gdistr(grid,data_val,sigma, inv2sigma2):
-
-    #Computes a gaussian with mean: data_val and width: sigma on the entire grid: grid
-
     dx = np.abs(grid[1] - grid[0])
     mu = data_val - grid[0]
     r = grid * 0.0
@@ -47,11 +48,12 @@ def make_histogram(grid,data):
     hist * norm
     return hist
 
+
 #Plotting the results
 grid = np.arange(xmin,xmax+1,bin_size)
 start = time.clock()
-y = make_histogram(grid,data_rand,'g')
-print "# Time taken : ", time.clock() - start, "(s)"
+y = make_histogram(grid,data)
+print "#Time taken : ", time.clock() - start, "(s)"
 
 plt.plot(grid,y,'b')
 plt.axis([xmin,xmax,0,1.2])
