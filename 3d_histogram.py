01977e704c2056cae4d4232c49113a51098c587b
Luca <luca@castor.home>
2017-11-15 15:29:45 +0100

    Contour doesn't work, don't know why

--- 3d_histogram.py
+++ 3d_histogram.py
@@ -1,37 +1,46 @@
 #Libraries
 import random
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import argparse
 import time
 
 
 #Variables for plotting
-parser = argparse.ArgumentParser()
-parser.add_argument("min", type=int, help="the lower x bound for plotting")
-parser.add_argument("max", type=int, help="the upper x bound for plotting")
-parser.add_argument("bins", type=int, help="the number of bins")
+'''parser = argparse.ArgumentParser()
+parser.add_argument("min", type=int, help="the lower bound for plotting (same for x,y and z)")
+parser.add_argument("max", type=int, help="the upper bound for plotting (same for x,y and z)")
+parser.add_argument("bins", type=int, help="the number of bins (same for x,y and z)")
 parser.add_argument("data", type=int, help="the number of data points")
-args = parser.parse_args()
+args = parser.parse_args()'''
 
-xmin = args.min
-xmax = args.max
+'''nmin = args.min
+nmax = args.max
 nbins = args.bins
 ndata = args.data
-bin_size = (xmax-xmin)/float(nbins)
+bin_size = (nmax-nmin)/float(nbins)'''
+
+nmin = 0
+nmax = 5
+nbins = 10
+ndata = 10
+bin_size = (nmax-nmin)/float(nbins)
 
 
 #Setting the data
-data = [random.gauss(6,1) for i in range(0,ndata)]
+data = np.random.multivariate_normal([2,4,3],np.identity(3)*0.1,ndata)
 
 
 #3d histogram fct for histogram
 #Computes a gaussian with mean: data_val and width: sigma on the entire grid: grid
-def gdistr(grid,data_val,sigma, inv2sigma2):
-    dx = np.abs(grid[1] - grid[0])
-    mu = data_val - grid[0]
-    r = grid * 0.0
-    r[int((mu - 4 * sigma) / dx + 1): int((mu + 4 * sigma) / dx + 1)] += np.exp(-(grid[int((mu - 4 * sigma) / dx + 1): int((mu + 4 * sigma) / dx + 1)] - mu)**2)
+def gd3DKernel(grid,mu,inv2sigma2):
+    nbpoints = len(grid)
+    r = [0]*len(grid)
+    for point in range(0, nbpoints):
+        prob = np.exp(-np.linalg.norm(grid[point]-mu)**2*inv2sigma2)
+        if prob>0.0005:
+            r[point] += prob
 
     return r
 
@@ -39,22 +48,35 @@ def gdistr(grid,data_val,sigma, inv2sigma2):
 #Making the histogram
 def make_histogram(grid,data):
     hist = [0]*len(grid)
-    sigma = 0.1
+    sigma = 1
     inv2sigma2 =  1.0 / (2.0 * sigma**2)
-    norm = 1.0 / np.sqrt(2.0 * np.pi * sigma**2)
+    norm = 1.0 / (np.sqrt(2.0 * np.pi * sigma**2)*float(ndata))
     for i in data:
-            hist += gdistr(grid,i,0.1,inv2sigma2)/float(nb_data)
+        hist += gd3DKernel(grid,i,inv2sigma2)
 
-    hist * norm
+    hist = [norm*x for x in hist]
     return hist
 
 
-#Plotting the results
-grid = np.arange(xmin,xmax+1,bin_size)
-start = time.clock()
-y = make_histogram(grid,data)
-print "#Time taken : ", time.clock() - start, "(s)"
+#Contour function
+def contour(c,hist,delta=0.1):
+    return [np.logical_and(hist < c+delta/2.0, hist > c-delta/2.0)]
 
-plt.plot(grid,y,'b')
-plt.axis([xmin,xmax,0,1.2])
+
+#Plotting the results
+fig = plt.figure()
+ax = Axes3D(fig)
+
+x = np.linspace(nmin,nmax,nbins)
+grid = np.vstack(np.meshgrid(x,x,x)).reshape(3,-1).T
+
+start = time.clock()
+hist = make_histogram(grid,data)
+print "#Time taken : ", time.clock() - start, "s"
+
+print hist
+print contour(0.01,hist)
+grid[contour(0.01,hist)]
+print len(grid)
+ax.scatter(grid[:,0],grid[:,1],grid[:,2])
 plt.show()
