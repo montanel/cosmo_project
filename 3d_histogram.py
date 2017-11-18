fd58324c2b19909f958b15ea352aca38ef42b90e
Luca <luca@castor.home>
2017-11-18 15:53:33 +0100

    Done even more optimisation, more than 10 times faster. Also checked with isoshpere function

--- 3d_histogram.py
+++ 3d_histogram.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python2
-
 #Libraries
 import random
 import matplotlib.pyplot as plt
@@ -9,7 +7,7 @@ import argparse
 import time
 
 
-#Variables for plotting
+#Initialization
 '''parser = argparse.ArgumentParser()
 parser.add_argument("min", type=int, help="the lower bound for plotting (same for x,y and z)")
 parser.add_argument("max", type=int, help="the upper bound for plotting (same for x,y and z)")
@@ -23,79 +21,98 @@ nbins = args.bins
 ndata = args.data
 bin_size = (nmax-nmin)/float(nbins)'''
 
-nmin = 0
-nmax = 5
-nbins = 10
-ndata = 10
+nmin = -2
+nmax = 2
+nbins = 100
+ndata = 1000
 bin_size = (nmax-nmin)/float(nbins)
+
+x = np.linspace(nmin,nmax,nbins)
+grid = np.vstack(np.meshgrid(x,x,x)).reshape(3,-1).T
 
 
 #Setting the data
-data = np.random.multivariate_normal([2,4,3],np.identity(3)*0.1,ndata)
+data = np.random.multivariate_normal([0,0,0],np.identity(3)*0.1,ndata)
 
 
-#3d gaussian kernel with mean: mu, factor(in exponential): inv2sigma2, calculated on the entire grid
+#3d gaussian kernel with mean: mu, factor(in exponential): inv2sigma2, calculated on the entire grid (benchmarks are with nbins=100,ndata=10)
 def gd3DKernel(grid,mu,inv2sigma2): #63.18s
-    r = np.zeros(len(grid))
+    hist = np.zeros(len(grid))
     for point in range(0,len(grid)):
         prob = np.exp(-np.linalg.norm(grid[point]-mu)**2*inv2sigma2)
         if prob > 0.0005:
-            r[point] += prob
+            hist[point] += prob
 
-    return r
+    return hist
 
-def gd3DKernelFaster(grid,mu,sigma,inv2sigma2): #64.49s
-    r = np.zeros(len(grid))
+def gd3DKernelFaster(grid,mu,sigma2,inv2sigma2): #64.49s
+    hist = np.zeros(len(grid))
     for point in range(0,len(grid)):
         norm = np.linalg.norm(grid[point]-mu)
-        if norm < 4*sigma:
-            prob = np.exp(-norm**2*inv2sigma2)
-            r[point] += prob
+        if norm < 4*sigma2:
+            hist[point] += np.exp(-norm**2*inv2sigma2)
 
-    return r
+    return hist
 
-def gd3DKernelEvenFaster(grid,norms_grid,mu,norm_mu,sigma,inv2sigma2): #41.53s
-    r = np.zeros(len(grid))
+def gd3DKernelEvenFaster(grid,norms_grid,mu,norm_mu,sigma2,inv2sigma2): #41.53s
+    hist = np.zeros(len(grid))
     for point in range(0,len(grid)):
         norm = np.sqrt(norms_grid[point]**2+norm_mu**2-2*np.dot(grid[point],mu))
-        if norm < 4*sigma:
-            prob = np.exp(-norm**2*inv2sigma2)
-            r[point] += prob
+        if norm < 4*sigma2:
+            hist[point] += np.exp(-norm**2*inv2sigma2)
 
-    return r
+    return hist
+
+def gd3DKernelFastest(grid,norms_grid,mu,norm_mu,sigma2,inv2sigma2): #3.93 s
+    hist = np.zeros(len(grid))
+    norm = np.sqrt(norms_grid**2+norm_mu**2-2*np.dot(grid,mu))
+    hist[np.less_equal(norm,4*sigma2)] += np.exp(-norm[np.less_equal(norm,4*sigma2)]**2*inv2sigma2)
+    return hist
 
 
 #Making the histogram
+#If we say that sigma2x=sigma2y=sigma2z=sigma2 in the covariance matrix and all
+#the other elements are 0, we can simplify det(covariance)=sigma2**3
 def make_histogram(grid,data):
     hist = np.zeros(len(grid))
-    sigma = (0.1)**0.5
-    inv2sigma2 = 1.0/(2.0*sigma**2)
-    norms_grid = [np.linalg.norm(i) for i in grid]
+    sigma2 = 1
+    inv2sigma2 = 1.0/(2.0*sigma2)
+    norms_grid = np.array([np.linalg.norm(i) for i in grid])
     for d in data:
         norm_d = np.linalg.norm(d)
-        hist += gd3DKernelEvenFaster(grid,norms_grid,d,norm_d,sigma,inv2sigma2)
+        hist += gd3DKernelFastest(grid,norms_grid,d,norm_d,sigma2,inv2sigma2)
 
-    hist = hist/(np.sqrt(2.0 * np.pi * sigma**2)**3 * float(ndata))
+    hist = hist/(np.sqrt((2.0*np.pi*sigma2)**3)*float(ndata))
     return hist
 
 
 #Contour function to find points of array: hist, with only the probabilty: c
-def contour(c,hist,delta=0.1):
+def contour(c,hist,delta):
     return [np.logical_and(hist < c+delta/2.0, hist > c-delta/2.0)]
 
+
+#Function to verify if histogram is well normalized
+def isosphere(c,delta):
+    ceff = c+delta/2.0
+    r = 2*0.1*np.log(ceff*np.sqrt((2*np.pi*0.1)**3))
+    phi, theta = np.mgrid[0.0:0.5*np.pi:180j, 0.0:2.0*np.pi:720j]
+    X = r*np.sin(phi)*np.cos(theta)
+    Y = r*np.sin(phi)*np.sin(theta)
+    Z = r*np.cos(phi)
+    ax.plot_surface(X,Y,Z)
+
+
+start = time.clock()
+hist = make_histogram(grid,data)
+print "#Time taken:", time.clock() - start, "s"
 
 #Plotting the results
 fig = plt.figure()
 ax = Axes3D(fig)
 
-x = np.linspace(nmin,nmax,nbins)
-grid = np.vstack(np.meshgrid(x,x,x)).reshape(3,-1).T
-
-start = time.clock()
-hist = make_histogram(grid,data)
-print "#Time taken : ", time.clock() - start, "s"
-
-grid = grid[contour(0.1,hist)]
-print len(grid)
+grid = grid[contour(0.1,hist,0.1)]
 ax.scatter(grid[:,0],grid[:,1],grid[:,2])
+ax.set_xlim(-1,1)
+ax.set_ylim(-1,1)
+ax.set_zlim(-1,1)
 plt.show()
