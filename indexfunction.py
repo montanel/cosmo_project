c885434e8bbaf5da7767337e8094d570939556e5
Luca <luca@castor.home>
2018-01-01 19:48:00 +0100

    Still trying to work it out

--- indexfunction.py
+++ indexfunction.py
@@ -8,6 +8,15 @@ def index(pt,grid):
     nbins = np.cbrt(len(grid))
     return int(np.around((pt[2]-nmin)/dx)+np.around((pt[0]-nmin)/dx*nbins,decimals=-2)+np.around((pt[1]-nmin)/dx*nbins**2,decimals=-4))
 
+def indexboolversion(pt,grid):
+    dx = abs(grid[1,2]-grid[0,2])
+    nmin = min(grid[:,2])
+    nbins = np.cbrt(len(grid))
+    index = int(np.around((pt[2]-nmin)/dx)+np.around((pt[0]-nmin)/dx*nbins,decimals=-2)+np.around((pt[1]-nmin)/dx*nbins**2,decimals=-4))
+    boolgrid = np.zeros(len(grid),dtype=bool)
+    boolgrid[index] = True
+    return boolgrid
+
 nmin = -1
 nmax = 1
 nbins = 100
@@ -15,5 +24,4 @@ nbins = 100
 x = np.linspace(nmin,nmax,nbins)
 grid = np.vstack(np.meshgrid(x,x,x)).reshape(3,-1).T
 norm = np.array(np.linalg.norm(grid,axis=1))
-print grid[np.less_equal(norm,1)]
-print index(grid[np.less_equal(norm,1)],grid)
+print grid[index([-0.5,0,0.66],grid)]
