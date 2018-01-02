47d991413758c6f9531e5e47a0316e6e94f17204
Luca <luca@castor.home>
2018-01-02 15:19:30 +0100

    Added a few things

--- tests.py
+++ tests.py
@@ -1,6 +1,8 @@
 #Libraries
 import numpy as np
 import time
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 def index(pt,grid):
     dx = abs(grid[1,2]-grid[0,2])
@@ -17,11 +19,24 @@ def indexboolversion(pt,grid):
     boolgrid[index] = True
     return boolgrid
 
+def inBox(grid,side,datapt):
+    return np.logical_and(np.logical_and(np.less_equal(abs(grid[:,0]-datapt[0]),side),np.less_equal(abs(grid[:,1]-datapt[1]),side)),np.less_equal(abs(grid[:,2]-datapt[2]),side))
+
 nmin = -1
 nmax = 1
-nbins = 100
+nbins = 50
 
 x = np.linspace(nmin,nmax,nbins)
 grid = np.vstack(np.meshgrid(x,x,x)).reshape(3,-1).T
-norm = np.array(np.linalg.norm(grid,axis=1))
-print grid[index([-0.5,0,0.66],grid)]
+datapt = [0,0,0]
+sigma = 0.1
+
+fig = plt.figure()
+ax = Axes3D(fig)
+
+grid = grid[inBox(grid,4*sigma,datapt)]
+ax.scatter(grid[:,0],grid[:,1],grid[:,2])
+ax.set_xlim(nmin,nmax)
+ax.set_ylim(nmin,nmax)
+ax.set_zlim(nmin,nmax)
+plt.show()
